ab392a4f49247dce960ccfd8d22b91467ad5929c
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2025-09-29 09:49:08 -0300

    refactor: mejorar generación de PDF en PDFViewer

--- src/PDFViewer.tsx
+++ src/PDFViewer.tsx
@@ -1,6 +1,8 @@
 import jsPDF from 'jspdf';
 import type { Receipt } from './interfaces/receipt.interface';
 
+
+
 const PDFViewer = (receipt: Receipt) => {
   const generatePDF = () => {
     const doc = new jsPDF();
@@ -8,10 +10,11 @@ const PDFViewer = (receipt: Receipt) => {
     doc.text(`Inquilino: ${receipt.tenantName}`, 10, 20);
     doc.text(`Monto: $${receipt.amount}`, 10, 30);
     doc.text(`Fecha: ${receipt.date}`, 10, 40);
-    
+    doc.setFontSize(10);
+    doc.text(`Gracias por su pago`, 10, 50);
     const currentDate = new Date().toISOString().replace(/[-:.]/g, '');
     const fileName = `recibo_${currentDate}.pdf`;
-    
+
     doc.save(`exports_pdf/${fileName}`);
   };
 
